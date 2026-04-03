185a937533e8fe731eba28a0e46d9626b787b1cb
Eric Lee <>
2026-04-03 18:49:23 +0800

    Replaced full-tree walk filter with WORKSPACE_DISCOVERY_EXCLUDES for resource search

--- packages/core/src/core/install/resource-search.ts
+++ packages/core/src/core/install/resource-search.ts
@@ -1,6 +1,6 @@
 import { basename, relative } from 'path';
-import { walkFiles } from '../../utils/file-walker.js';
-import { isExcludedFromPackage } from '../../utils/package-filters.js';
+import { walkFiles } from '../../utils/fs.js';
+import { WORKSPACE_DISCOVERY_EXCLUDES } from '../../constants/workspace.js';
 import { getResourceTypeDef } from '../resources/resource-registry.js';
 import type { ResourceTypeId } from '../../types/resources.js';
 
@@ -8,17 +8,6 @@ function hasAncestorDirNamed(basePath: string, filePath: string, dirName: string
   const relativePath = relative(basePath, filePath).replace(/\\/g, '/');
   const segments = relativePath.split('/').filter(Boolean);
   return segments.slice(0, -1).includes(dirName);
-}
-
-function createPackageDiscoveryFilter(basePath: string) {
-  return (entryPath: string): boolean => {
-    const relativePath = relative(basePath, entryPath).replace(/\\/g, '/');
-    if (!relativePath || relativePath === '.') {
-      return true;
-    }
-
-    return !isExcludedFromPackage(relativePath);
-  };
 }
 
 export async function findMarkerResourceFiles(
@@ -31,9 +20,8 @@ export async function findMarkerResourceFiles(
   }
 
   const files: string[] = [];
-  const filter = createPackageDiscoveryFilter(basePath);
 
-  for await (const file of walkFiles(basePath, { filter })) {
+  for await (const file of walkFiles(basePath, [], { excludeDirs: WORKSPACE_DISCOVERY_EXCLUDES })) {
     if (basename(file) !== definition.marker) {
       continue;
     }
